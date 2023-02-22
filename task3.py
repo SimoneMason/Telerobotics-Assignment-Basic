c27b41d83062caf10c0f015225edf8565b9d65b3
SimoneMason <125269142+SimoneMason@users.noreply.github.com>
2023-02-22 16:39:32 +0000

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,6 @@
 # Task 3. To use keyboard input to control the angle of the servos, find the limiting angle of each servo
-# and grab a small item by the robot hand (after grabbing, move the robot tank to another position to put
-# the item down). You may take a video via the camera in the front to show the grabbing, moving, and
-# releasing of a small item (e.g. an empty plastic water bottle). (20/100)
+# and grab a small item by the robot hand. Take a video via the camera in the front to show the grabbing, moving, and
+# releasing of a small item 
 
 from _XiaoRGEEK_SERVO_ import XR_Servo
 Servo = XR_Servo()
@@ -38,9 +37,7 @@ def moveServo1(angle, offset):
     else: angle = angle - offset
     
     return angle
-    
-    
-
+       
 
 def moveServo2(angle, offset):
 
